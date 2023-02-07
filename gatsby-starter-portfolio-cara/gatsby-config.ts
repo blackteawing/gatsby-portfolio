6987509077c9597701dc310964d9907dced89784
美国短毛猫NEMO <azraelwing@gmail.com>
2023-02-07 17:50:33 +1100

    gatsby portfolio

---
+++ gatsby-starter-portfolio-cara/gatsby-config.ts
@@ -0,0 +1,63 @@
+import type { GatsbyConfig, PluginRef } from "gatsby"
+import "dotenv/config"
+
+const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    // You can overwrite values here that are used for the SEO component
+    // You can also add new values here to query them like usual
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
+    siteTitle: `My Portfolio`,
+    siteTitleAlt: `My Portfolio`,
+    siteHeadline: `My Portfolio`,
+    siteUrl: `https://newskycn.com/`,
+    siteDescription: `A portfolio website about JavaScript and React`,
+    siteImage: `/banner.jpg`,
+    author: `@Vincent`,
+  },
+  trailingSlash: `never`,
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-cara`,
+      // See the theme's README for all available options
+      options: {},
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Vincent-My Portfolio`,
+        short_name: `Vincent`,
+        description: `A portfolio website about JavaScript and React`,
+        start_url: `/`,
+        background_color: `#141821`,
+        // This will impact how browsers show your PWA/website
+        // https://css-tricks.com/meta-theme-color-and-trickery/
+        // theme_color: `#f6ad55`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    shouldAnalyseBundle && {
+      resolve: `gatsby-plugin-webpack-bundle-analyser-v2`,
+      options: {
+        analyzerMode: `static`,
+        reportFilename: `_bundle.html`,
+        openAnalyzer: false,
+      },
+    },
+  ].filter(Boolean) as Array<PluginRef>,
+}
+
+export default config
